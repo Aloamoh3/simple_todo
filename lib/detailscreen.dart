5a7618ad728934e518dc061d0044a2327583977a
Aloamoh3 <aloamoh@icloud.com>
2025-08-02 20:52:16 -0400

    Update detailscreen.dart

--- lib/detailscreen.dart
+++ lib/detailscreen.dart
@@ -1,14 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:simple_todo/edit_todo.dart';
 import 'package:simple_todo/todo.dart';
 
-class DetailScreen extends StatelessWidget {
+class DetailScreen extends StatefulWidget {
   const DetailScreen({super.key});
 
   @override
+  State<DetailScreen> createState() => _DetailScreenState();
+}
+
+class _DetailScreenState extends State<DetailScreen> {
+
+late Todo todo;
+
+@override
+void initState() {
+  super.initState();
+  todo = ModalRoute.of(context)!.settings.arguments as Todo;
+}
+
+  @override
   Widget build(BuildContext context) {
-    final todo = ModalRoute.of(context)!.settings.arguments as Todo;
+    
     return Scaffold(appBar: AppBar(title: Text(todo.title),),
-    body: Padding(padding: const EdgeInsets.all(16), child: Text(todo.description),),
+    body: Padding(
+      padding: const EdgeInsets.all(16),
+       child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Text(todo.description),
+        ElevatedButton(onPressed: () {
+          Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const EditTodo(),
+                        settings: RouteSettings(arguments: todo),
+                      ),
+                    );
+        }, child: Text("Edit Todo"),
+            ),
+       ]),
+     ),
     );
   }
 }
