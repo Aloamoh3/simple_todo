5547e8e163f103ec1554bac24c45f1e51cbbc514
Aloamoh3 <aloamoh@icloud.com>
2025-08-05 20:01:42 -0400

    Update helper.dart

--- lib/helper.dart
+++ lib/helper.dart
@@ -1,1 +1,31 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:simple_todo/todo.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
+Future<void> serializeTODO(List<String> savedTodo, List<Todo> todos) async {
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  savedTodo.clear();
+  for (int i = 0; i < todos.length; i++) {
+    String todo = json.encode(todos[i]);
+    savedTodo.add(todo);
+  }
+    await prefs.setStringList("todo", savedTodo);
+  }
+
+
+Future<List<Todo>> deserializeTodo(
+  List<String> savedTodo, List<Todo> todos) async {
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  var fromMem = prefs.getStringList("todo");
+ if (fromMem != null) {
+  todos.clear();
+  savedTodo = fromMem;
+  for (int i = 0; i < savedTodo.length; i++) {
+    Todo todo = Todo.fromJson(json.decode(savedTodo[i]));
+      todos.add(todo);
+      
+  }
+ }
+ return todos;
+}
